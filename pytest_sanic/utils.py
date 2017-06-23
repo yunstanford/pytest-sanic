ef6689357dc17e394b88b3f5c07b78037b3a477b
Yun Xu <yunx@zillowgroup.com>
2017-06-23 00:27:39 -0700

    working on TestServer

--- pytest_sanic/utils.py
+++ pytest_sanic/utils.py
@@ -1,48 +1,143 @@
 from aiohttp import ClientSession
-from sanic.server import serve
+from sanic.server import serve, trigger_events
+from sanic.app import Sanic
 import socket
 
 
 class TestServer:
 
-	def __init__(self, app, host='127.0.0.1', loop=None, **kwargs):
-		self.server = None
-		self.port = None
+	"""
+	a test server class designed for easy testing in Sanic-based Application.
+	"""
+
+	def __init__(self, app, host='127.0.0.1',
+				 loop=None, protocol=None,
+				 backlog=100, ssl=None,
+				 scheme=None,
+				 **kwargs):
+		self.app = app
 		self.loop = loop
 		self.host = host
-		self.root = None
+		self.protocol = protocol
+		self.backlog = backlog
+		self.server = None
+		self.port = None
+		self.ssl = ssl
+		if self.scheme is None:
+			if self.ssl:
+				self.scheme = "https"
+			else:
+				self.scheme = "http"
 
 	async start_server(self, loop=None, **kwargs):
 		self.loop = loop
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.bind((self.host, 0))
 		self.port = self.socket.getsockname()[1]
-		if kwargs.get('ssl', None):
-			self.scheme = 'https'
-		else:
-			self.scheme = 'http'
 
-		# before start
+		# server settings
+		server_settings = self.app._helper(
+            host=self.host, port=self.port,
+            ssl=self.ssl, sock=self.sock,
+            loop=self.loop, protocol=self.protocol,
+            backlog=self.backlog, run_async=True)
 
-		# after start
+		# Let's get listeners
+		self.before_server_start = server_settings.get('before_start', [])
+		self.after_server_start = server_settings.get('after_start', [])
+		self.before_server_stop = server_settings.get('before_stop', [])
+		self.after_server_stop = server_settings.get('after_stop', [])
+
+		# Trigger before_start events
+		trigger_events(self.before_server_start, self.loop)
 
 		# start server
+        self.server = await serve(**server_settings)
+        self.is_running = True
 
+        # Trigger after_start events
+        trigger_events(self.after_server_start, self.loop)
 
 	async close(self):
-		# before stop
-		pass
-		# stop
+		"""
+		Close server.
+		"""
+		if self.is_running and not self.closed
+			# Trigger before_stop events
+			trigger_events(self.before_server_stop, self.loop)
 
-		# after stop and clean up
+			# Stop Server
+			self.server.close()
+			await self.server.wait_closed()
 
+			# Trigger after_stop events
+			trigger_events(self.after_server_stop, self.loop)
+
+			self.closed = True
+			self.port = None
+
+	def is_running(self):
+		"""
+		Check if server is running.
+		"""
+		return self.server is not None
+
+	def make_url(self, uri):
+		return "{scheme}://{host}:{port}{uri}".format(
+				scheme=self.scheme,
+				host=self.host,
+				port=self.port,
+				uri=uri
+			)
+
+	@property
+	def before_server_start(self):
+		return self.before_server_start
+
+	@property
+	def after_server_start(self):
+		return self.after_server_start
+
+	@property
+	def before_server_stop(self):
+		return self.before_server_stop
+
+	@property
+	def after_server_stop(self):
+		return self.after_server_stop
 
 
 class TestClient:
 
 	"""
-	a test client class designed for easy testing in Sanic Application.
+	a test client class designed for easy testing in Sanic-based Application.
 	"""
 
-	def __init__(self, app, host):
-		pass
+	def __init__(self, app, host, loop):
+		self._app = app
+		self._loop = loop
+
+	@property
+	def app(self):
+		return self._app
+	
+	@property
+	def host(self):
+		return self._host
+	
+	@property
+	def port(self):
+		return self._port
+	
+	@property
+	def server(self):
+		return self._server
+	
+	@property
+	def session(self):
+		return self._session
+
+	async def start_server(self):
+		await self._server.start_server(loop=self.loop)
+	
+	
