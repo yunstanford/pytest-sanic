8152b6a9166191dc30e69f226d187c23a2e7f9a8
Yun Xu <yunx@zillowgroup.com>
2017-07-07 21:21:46 -0700

    publish 0.1.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
 tests_require = []
 
 setup(name='pytest-sanic',
-      version='0.1.1',
+      version='0.1.2',
       description='',
       long_description=open(README_PATH).read(),
       author='Yun Xu',
