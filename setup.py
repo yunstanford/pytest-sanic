953ac5439b81630b04d631d2c4fcf5cb45d1c36b
Yun Xu <yunx@zillowgroup.com>
2018-02-12 13:13:06 -0800

    upgrade aiohttp and bump up version to 0.1.7

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
 tests_require = []
 
 setup(name='pytest-sanic',
-      version='0.1.6',
+      version='0.1.7',
       description='',
       long_description=open(README_PATH).read(),
       author='Yun Xu',
